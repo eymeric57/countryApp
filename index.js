a6f53fca093f086a3e8b9256f62320f66d11efd0
unknown <eymeric57160@hotmail.fr>
2023-12-08 19:45:09 +0100

    tp country in progress

--- index.js
+++ index.js
@@ -3,43 +3,48 @@
 // 2 - Créer une fonction pour "fetcher" les données, afficher les données dans la console.
 
 // 3 - Passer les données à une variable
+var countryCt = document.querySelector(".countries-container")
 let country = [];
 
- async function fetchCountry() {
-
-   await fetch("https://restcountries.com/v3.1/all")
+async function fetchCountry() {
+  await fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
-    .then((data) =>(country = data));
+    .then((data) => (country = data));
 
-    for (let i = 0; i < 250; i++)
-    console.log(country[i]);
-    
+  for (let i = 0; i < 250; i++) console.log(country[i]);
 }
 
-
-
 const countryDisplay = async () => {
-    await fetchCountry();
+  await fetchCountry();
 
-    document.body.innerHTML = country.map(
-    (countries) => 
-    `
-    <div class="countries-container">
-        <div class="country">
+ countryCt.innerHTML = country
+    .map(
+      (countries) =>
+        `
+
+    <div class="countryBox">
+    <div class="country">
+        <div class="flag">
             <img src="${countries.flags.png}">
-
-                <p>${countries.name}</p>
+        </div>
+        <div class="countryName">
+                <h2>${countries.name.common}</h2>
+        </div>
+        <div class="capital">
+                <h3>${countries.capital}</h3>
+        </div>
+        <div class="population">
+        <p>Population : ${countries.population}</p>
+        </div>
         </div>
     </div>
     
     `
-    
     )
-    join()
-}
+    .join("");
+};
 
-countryDisplay()
-
+countryDisplay();
 
 // 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
 
